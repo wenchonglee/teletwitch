d9a6e6e0abae7a39658ef6bbfd1fa1e1a3ebe16f
wenchonglee <18256786+wenchonglee@users.noreply.github.com>
2023-10-22 16:46:49 +0800

    added try-catch for debugging

--- src/routes/api/sticker-set/+server.ts
+++ src/routes/api/sticker-set/+server.ts
@@ -102,48 +102,51 @@ export const PUT: RequestHandler = async ({ request }) => {
     async start(controller) {
       controller.enqueue(encoder.encode("hello"));
 
-      const form = await request.formData();
-      const stickerUrl = form.get("stickerUrl") as string;
+      try {
+        const form = await request.formData();
+        const stickerUrl = form.get("stickerUrl") as string;
 
-      for (const pair of form.entries()) {
-        console.log(`${pair[0]}, ${pair[1]}`);
+        for (const pair of form.entries()) {
+          console.log(`${pair[0]}, ${pair[1]}`);
+        }
+        const tempName = Date.now().toString();
+        controller.enqueue(encoder.encode("downloading video"));
+
+        const emote = await get7tvEmote(stickerUrl);
+        controller.enqueue(encoder.encode("resizing video"));
+        const resizedWebpFileName = await resizeWebp(tempName, emote);
+        // todo: if not animated, stop here
+
+        controller.enqueue(encoder.encode("slicing video to images "));
+        await convertWebpToFrames(tempName, resizedWebpFileName);
+        controller.enqueue(encoder.encode("stitching images to webm"));
+        const webmFileName = await convertFramesToWebm(tempName);
+        await cleanup(tempName);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+
+        controller.enqueue(encoder.encode("uploading sticker file"));
+        const uploadResponse = await uploadStickerFile({
+          filePath: webmFileName,
+          stickerFormat: form.get("stickerFormat") as "video",
+          userId: form.get("userId") as string,
+        });
+        console.log(uploadResponse);
+        if (!uploadResponse) {
+          return json("oh no");
+        }
+        // https://github.com/sveltejs/kit/issues/5344
+
+        controller.enqueue(encoder.encode("adding to stickerset"));
+        const newStickerResponse = await addStickerToSet({
+          stickerFileId: uploadResponse.result.file_id,
+          emojiList: ["🤣"],
+          name: form.get("stickerSetName") as string,
+          userId: form.get("userId") as string,
+        });
+        controller.enqueue(encoder.encode("sticker added!"));
+      } catch (err) {
+        console.log(err);
       }
-      const tempName = Date.now().toString();
-      controller.enqueue(encoder.encode("downloading video"));
-
-      const emote = await get7tvEmote(stickerUrl);
-      controller.enqueue(encoder.encode("resizing video"));
-      const resizedWebpFileName = await resizeWebp(tempName, emote);
-      // todo: if not animated, stop here
-
-      controller.enqueue(encoder.encode("slicing video to images "));
-      await convertWebpToFrames(tempName, resizedWebpFileName);
-      controller.enqueue(encoder.encode("stitching images to webm"));
-      const webmFileName = await convertFramesToWebm(tempName);
-      await cleanup(tempName);
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-
-      controller.enqueue(encoder.encode("uploading sticker file"));
-      const uploadResponse = await uploadStickerFile({
-        filePath: webmFileName,
-        stickerFormat: form.get("stickerFormat") as "video",
-        userId: form.get("userId") as string,
-      });
-      console.log(uploadResponse);
-      if (!uploadResponse) {
-        return json("oh no");
-      }
-      // https://github.com/sveltejs/kit/issues/5344
-
-      controller.enqueue(encoder.encode("adding to stickerset"));
-      const newStickerResponse = await addStickerToSet({
-        stickerFileId: uploadResponse.result.file_id,
-        emojiList: ["🤣"],
-        name: form.get("stickerSetName") as string,
-        userId: form.get("userId") as string,
-      });
-      controller.enqueue(encoder.encode("sticker added!"));
-
       // console.log(newStickerResponse);
       controller.close();
     },
