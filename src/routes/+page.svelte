c330bd896cb837216e71f1147d5df78210fdf113
wenchonglee <18256786+wenchonglee@users.noreply.github.com>
2023-10-23 22:29:52 +0800

    auth experiment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,6 +26,28 @@
       <a href={`/sticker-set/${stickerSetName}`}>Update a sticker set</a>
     {/if}
   </div>
+  <script
+    async
+    src="https://telegram.org/js/telegram-widget.js?22"
+    data-telegram-login="teletwitchsticker_bot"
+    data-size="large"
+    data-onauth="onTelegramAuth(user)"
+  ></script>
+  <script type="text/javascript">
+    function onTelegramAuth(user) {
+      console.log(JSON.stringify(user));
+      alert(
+        "Logged in as " +
+          user.first_name +
+          " " +
+          user.last_name +
+          " (" +
+          user.id +
+          (user.username ? ", @" + user.username : "") +
+          ")"
+      );
+    }
+  </script>
 </main>
 
 <style>
